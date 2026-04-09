01f693e344747b41979b8ac381c4e9d63d7be505
Thomas Vincent <thomasvincent@gmail.com>
2026-04-09 01:44:49 -0700

    refactor: replace ExtJS 2.2 with Cacti-native jQuery UI
    Remove vendored ExtJS 2.2 framework (2.5MB) and all ExtJS app code.
    Replace with Cacti-idiomatic jQuery using standard page structure,
    html_start_box/html_end_box table patterns, and jQuery UI dialogs.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- controllers/layout.php
+++ controllers/layout.php
@@ -1,87 +1,64 @@
 <?php
 
-class NpcLayoutController extends controller {
-    var $params = array();
+class NpcLayoutController extends Controller {
+	var $params = array();
 
-    function drawFrame($params) {
-        $config = $params['config'];
-
-		$this->drawCommonFrame($config['url_path'] . 'plugins/npc/npc.php?module=layout&action=drawLayout', $params);
-    }
-
-    function drawLayout($params) {
+	function drawFrame($params) {
 		$config = $params['config'];
-		$npc_base         = $config['url_path'] . 'plugins/npc/js/';
-		$npc_ext_base     = $config['url_path'] . 'plugins/npc/js/ext/';
-		$npc_css_base     = $config['url_path'] . 'plugins/npc/css/';
+
+		general_header();
+
+		$npc_base = $config['url_path'] . 'plugins/npc/';
+
+		$current_tab = 'dashboard';
+		if (isset_request_var('tab')) {
+			$current_tab = get_request_var('tab');
+		}
+
+		$tabs = array(
+			'dashboard'  => __('Dashboard', 'npc'),
+			'hosts'      => __('Hosts', 'npc'),
+			'services'   => __('Services', 'npc'),
+			'comments'   => __('Comments', 'npc'),
+			'downtime'   => __('Downtime', 'npc'),
+			'eventlog'   => __('Event Log', 'npc'),
+			'process'    => __('Process Info', 'npc'),
+		);
+
+		/* draw the tabs */
+		print "<div class='tabs' style='margin-bottom:0px;'><nav><ul role='tablist'>\n";
+
+		foreach ($tabs as $tab_short => $tab_name) {
+			print "<li role='tab' tabindex='0'><a " .
+				(($tab_short == $current_tab) ? "class='selected'" : '') .
+				" href='" . html_escape($npc_base . 'npc.php?module=layout&action=drawFrame&tab=' . $tab_short) . "'>" .
+				html_escape($tab_name) . '</a></li>';
+		}
+
+		print "</ul></nav></div>\n";
+
+		/* pass config to JS */
 		?>
-
-		<script type='text/javascript' src='<?php echo $config['url_path']; ?>include/js/jquery.js'></script>
-
-		<link rel='stylesheet' type='text/css' href='<?php echo $npc_ext_base; ?>resources/css/ext-all.css' />
-		<link rel='stylesheet' type='text/css' href='<?php echo $npc_ext_base; ?>resources/css/xtheme-slate.css' />
-		<link rel='stylesheet' type='text/css' href='<?php echo $npc_css_base; ?>main.css' />
-		<!-- <link rel="stylesheet" type="text/css" href="<?php echo $npc_ext_base; ?>resources/css/xtheme-darkgray.css" /> -->
-		<!-- <link rel="stylesheet" type="text/css" href="<?php echo $npc_ext_base; ?>resources/css/xtheme-gray.css" /> -->
-		<link rel='stylesheet' type='text/css' href='<?php echo $npc_css_base; ?>ext-ux-livegrid.css' />
-
-		<script type='text/javascript' src='<?php echo $npc_ext_base; ?>adapter/ext/ext-base.js'></script>
-		<script type='text/javascript' src='<?php echo $npc_ext_base; ?>ext-all.js'></script>
-		<script type='text/javascript' src='<?php echo $npc_base; ?>npc-all-min.js'></script>
-		<script type='text/javascript' src='<?php echo $npc_base; ?>portlets-all-min.js'></script>
 		<script type='text/javascript'>
-
-		// Add some properties to the params array
-		npc.params.npc_portlet_refresh = <?php echo read_config_option('npc_portlet_refresh'); ?>;
-		npc.params.npc_date_format     = '<?php echo read_config_option('npc_date_format'); ?>';
-		npc.params.npc_time_format     = '<?php echo read_config_option('npc_time_format'); ?>';
-		npc.params.npc_nagios_url      = '<?php echo read_config_option('npc_nagios_url'); ?>';
-		npc.params.userName            = '<?php echo db_fetch_cell_prepared('SELECT username FROM user_auth WHERE id = ?', array($_SESSION['sess_user_id'])); ?>';
-		npc.params.npc_host_icons      = '<?php echo read_config_option('npc_host_icons'); ?>';
-		npc.params.npc_service_icons   = '<?php echo read_config_option('npc_service_icons'); ?>';
-
-		npc.params.cacti_path          = '<?php echo URL_PATH; ?>';
-		var strLen = npc.params.cacti_path;
-		if (npc.params.cacti_path.charAt(strLen-1) == '/') {
-			npc.params.cacti_path = npc.params.cacti_path.slice(0,strLen-1);
-		}
-
-		<?php $state = unserialize(db_fetch_cell_prepared('SELECT settings FROM npc_settings WHERE user_id = ?', array($_SESSION['sess_user_id']))); ?>
-		var ExtState = Ext.decode('<?php echo json_encode($state); ?>');
-
-		// Launch the app
-		Ext.onReady(npc.init, npc);
-
-		Ext.onReady(function() {
-			Ext.state.Manager.setProvider(new Ext.state.HttpProvider({url: 'npc.php?module=settings&action=save'}));
-			Ext.QuickTips.init();
-			npc.initPortlets();
-			window.parent.npcSize();
-		});
-
-		Ext.Ajax.on('beforerequest', function(conn, options) {
-			options.params.__csrf_magic = window.parent.getMagicToken();
-		});
-
-		function readySize() {
-			window.parent.npcSize();
-		}
-
-		$(function() {
-			setTimeout(readySize, 350);
-		});
-
+		var npcConfig = {
+			baseUrl:          '<?php echo $npc_base; ?>',
+			npcUrl:           '<?php echo $npc_base; ?>npc.php',
+			portletRefresh:   <?php echo (int) read_config_option('npc_portlet_refresh'); ?>,
+			dateFormat:       '<?php echo addslashes(read_config_option('npc_date_format')); ?>',
+			timeFormat:       '<?php echo addslashes(read_config_option('npc_time_format')); ?>',
+			nagiosUrl:        '<?php echo addslashes(read_config_option('npc_nagios_url')); ?>',
+			userName:         '<?php echo addslashes(db_fetch_cell_prepared('SELECT username FROM user_auth WHERE id = ?', array($_SESSION['sess_user_id']))); ?>',
+			hostIcons:        '<?php echo read_config_option('npc_host_icons'); ?>',
+			serviceIcons:     '<?php echo read_config_option('npc_service_icons'); ?>',
+			currentTab:       '<?php echo addslashes($current_tab); ?>',
+			csrfToken:        '<?php echo csrf_get_tokens(); ?>'
+		};
 		</script>
-		<div id='north'>
-			<div id='msg-div'></div>
-		</div>
-		<div id='west'>
-			<div id='west-monitoring'></div>
-			<div id='west-config'></div>
-		</div>
-		<div id='props-panel' style='width:200px;height:200px;overflow:hidden;'></div>
-		<div id='south'></div>
+		<script type='text/javascript' src='<?php echo $npc_base; ?>js/npc.js'></script>
 		<?php
-    } // end drawLayout
-}
 
+		print "<div id='npc-content'></div>\n";
+
+		bottom_footer();
+	}
+}
