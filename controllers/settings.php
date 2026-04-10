70e8d418a6d505b05521e896a6271eabaf58bbe4
Thomas Vincent <thomasvincent@gmail.com>
2026-04-10 07:06:22 -0700

    fix(security): pass allowed_classes to unserialize
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- controllers/settings.php
+++ controllers/settings.php
@@ -42,7 +42,7 @@ class NpcSettingsController extends Controller {
         $user_id = $_SESSION['sess_user_id'];
         $obj = $this->getSettings($user_id);
 
-        $settings = unserialize($obj->settings);
+        $settings = @unserialize($obj->settings, ["allowed_classes" => false]);
 		if (isset($params['name'])) {
 	        $settings[$params['name']] = $params['value'];
 		}
