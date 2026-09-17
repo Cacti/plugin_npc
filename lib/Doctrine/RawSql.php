743f891d4958b2f46fa1d1382f2b4e3f7e84e98e
TheWitness <thewitness@cacti.net>
2026-09-17 13:17:53 -0400

    fix: use break instead of continue in switch inside foreach (PHP warning)

--- lib/Doctrine/RawSql.php
+++ lib/Doctrine/RawSql.php
@@ -163,7 +163,7 @@ class Doctrine_RawSql extends Doctrine_Query_Abstract
                     }
                     break;
                 case 'by':
-                    continue;
+                    break;
                 default:
                     //not a keyword so we add it to the previous type.
                     if ( ! isset($parts[$type][0])) {
